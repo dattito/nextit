9937a38d561d7cc3013689b91498087a8e27cf3a
i568756 <elisabeth.barar@sap.com>
2023-12-13 16:16:06 +0100

    bind in logo

--- web/components/navabar.js
+++ web/components/navabar.js
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
 export default function Navbar() {
   return (
     <nav class="navbar bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand">Ihr Logo</a>
+        <a class="navbar-brand">
+          <Image src="/Logo_wide.png" width={100} height={100} />
+        </a>
         <form class="d-flex" role="search">
           <button class="btn btn-outline-success" type="submit">
             Log in
